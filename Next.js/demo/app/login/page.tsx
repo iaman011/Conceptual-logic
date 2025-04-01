c052b80c603ececb474f457dff0265afc663772e
iaman011 <iaman.singh011@gmail.com>
2025-04-01 19:56:12 +0530

    changes commit

---
+++ Next.js/demo/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Loggedin from "@/Components/Signin/page"
+
+export default function login(){
+    return(
+        <div>
+        <Loggedin />
+    </div>
+    );
+   
+}
---
+++ Next.js/demo/app/signup/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Signup from "@/Components/Signup/page";
+
+export default function Signup01() {
+    return (
+<div>
+  <Signup />
+</div>
+    );
+  }
+  
